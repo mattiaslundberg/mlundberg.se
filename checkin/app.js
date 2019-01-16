99c2ecdbce034b749836811620caec4783cbfd7c
Mattias Lundberg <mattias@kundo.se>
2019-01-16 18:54:14 +0100

    Avoid reusing questions

--- checkin/app.js
+++ checkin/app.js
@@ -1,17 +1,46 @@
+const LOCALSTORAGE_USED = "used_question_ids"
+
+const random = (maxValue) => Math.round(Math.random() * maxValue);
+
+const getRandomNumber = (maxValue, usedValues) => {
+  if (usedValues.length >= maxValue) {
+    return random(maxValue);
+  }
+  let candidate = 0;
+  let remainingTries = 300;
+  while (!candidate && usedValues.includes(candidate) && --remainingTries > 0) {
+    candidate = random(maxValue);
+  }
+  return candidate;
+};
+
+const saveToLocalStorage = (usedQuestionIds) => {
+  if (!localStorage) { return; }
+  localStorage.setItem(LOCALSTORAGE_USED, JSON.stringify(usedQuestionIds));
+};
+
 const vm = new Vue({
   el: '#app',
   data: {
     questions: [],
+    usedQuestionIds: [],
     loading: true,
     question: "",
   },
   created: function() {
+    if (localStorage) {
+      const usedIds = JSON.parse(localStorage.getItem(LOCALSTORAGE_USED) || "[]");
+      this.usedQuestionIds.push.apply(this.usedQuestionIds, usedIds);
+    }
+
     fetch("questions")
       .then(data => data.text())
       .then(data => {
         const newQuestions = data.trim().split("\n");
         this.questions.push.apply(this.questions, newQuestions);
-        this.questionIndex = Math.round(Math.random() * this.questions.length);
+        this.questionIndex = getRandomNumber(this.questions.length, this.usedQuestionIds)
+        this.usedQuestionIds.push(this.questionIndex)
+        saveToLocalStorage(this.usedQuestionIds);
         this.loading = false;
         this.question = "NEW_VALUE";
       }).catch(e => console.warn(e))
